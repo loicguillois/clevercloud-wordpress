6aa2288c80d6ff6b4f7eb4f8fcc5daee19b634fd
RENAN SAVIDAN <renansavidan@users.noreply.github.com>
2021-05-03 18:21:01 -0300

    customize Woo Checkout fields for demo 1

--- web/app/themes/hello-theme-child/functions.php
+++ web/app/themes/hello-theme-child/functions.php
@@ -21,3 +21,34 @@ function hello_elementor_child_enqueue_scripts() {
 	);
 }
 add_action( 'wp_enqueue_scripts', 'hello_elementor_child_enqueue_scripts', 20 );
+
+
+
+/* Csutom WooCommerce checkout */
+
+// Billing and shipping addresses fields
+add_filter( 'woocommerce_default_address_fields' , 'filter_default_address_fields', 20, 1 );
+function filter_default_address_fields( $address_fields ) {
+    // Only on checkout page
+    if( ! is_checkout() ) return $address_fields;
+
+    // All field keys in this array
+    $key_fields = array('country','first_name','last_name','company','address_1','address_2','city','state','postcode');
+
+    // Loop through each address fields (billing and shipping)
+    foreach( $key_fields as $key_field )
+        $address_fields[$key_field]['required'] = false;
+
+    return $address_fields;
+}
+
+// For billing email and phone - Make them not required
+add_filter( 'woocommerce_billing_fields', 'filter_billing_fields', 20, 1 );
+function filter_billing_fields( $billing_fields ) {
+    // Only on checkout page
+    if( ! is_checkout() ) return $billing_fields;
+
+    $billing_fields['billing_phone']['required'] = false;
+    //$billing_fields['billing_email']['required'] = false;
+    return $billing_fields;
+}
