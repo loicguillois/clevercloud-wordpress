d03c2ccc3f48d102c950f6d1b19f4e1ea6e2f0c5
renansavidan <renansavidan@gmail.com>
2021-06-06 12:47:28 -0500

    my account one page code deep modif test 1

--- web/app/themes/hello-theme-child/woocommerce/myaccount/my-account.php
+++ web/app/themes/hello-theme-child/woocommerce/myaccount/my-account.php
@@ -15,17 +15,37 @@
  * @version 3.5.0
  */
 
+
 defined( 'ABSPATH' ) || exit;
 
-/**
- * My Account navigation.
- *
- * @since 2.6.0
- */
-//do_action( 'woocommerce_account_navigation' ); ?>
+$standard=2;
+if ($standard==1){
+	/****************************** standard ***************************************/
+	/**
+	 * My Account navigation.
+	 *
+	 * @since 2.6.0
+	 */
+	do_action( 'woocommerce_account_navigation' ); ?>
 
-<div class="woocommerce-MyAccount-content">
-	<h2>My Account custom </h2> 
+	<div class="woocommerce-MyAccount-content">
+	<?php
+		/**
+	 * My Account content.
+	 *
+	 * @since 2.6.0
+	 */
+		do_action( 'woocommerce_account_content' );
+	?>
+	</div>
+	<?php
+
+}
+else if ($standard==2){
+/****************************** custom ***************************************/
+	?>
+	<div class="woocommerce-MyAccount-content">
+	<h2>Mon Compte Custom</h2> 
 	<?php
 		/**
 		 * My Account content.
@@ -33,16 +53,31 @@ defined( 'ABSPATH' ) || exit;
 		 * @since 2.6.0
 		 */
 
-		//add_action( 'woocommerce_account_navigation', 'woocommerce_account_navigation' );
-		//add_action( 'woocommerce_account_content', 'woocommerce_account_content' );
+			//add_action( 'woocommerce_account_navigation', 'woocommerce_account_navigation' );
+			//add_action( 'woocommerce_account_content', 'woocommerce_account_content' );
 
-		do_action( 'woocommerce_account_edit-address_endpoint', 'woocommerce_account_edit_address' );
-		do_action( 'woocommerce_account_edit-account_endpoint', 'woocommerce_account_edit_account' );
-		do_action( 'woocommerce_account_payment-methods_endpoint', 'woocommerce_account_payment_methods' );
-		do_action( 'woocommerce_account_add-payment-method_endpoint', 'woocommerce_account_add_payment_method' );
-		do_action( 'woocommerce_account_orders_endpoint', 'woocommerce_account_orders' );
-		do_action( 'woocommerce_account_view-order_endpoint', 'woocommerce_account_view_order' );
+			//do_action( 'woocommerce_account_edit-address_endpoint', 'woocommerce_account_edit_address' );
+			
+			
+			
+			do_action( 'woocommerce_account_edit-account_endpoint', 'woocommerce_account_edit_account' );
+			//do_action( 'woocommerce_account_subscriptions_endpoint', 'woocommerce_account_edit_account' );
+			//do_action( 'woocommerce_account_view-subscription_endpoint', 'woocommerce_account_edit_account' );
+			$subscriptions  = wcs_get_users_subscriptions();
 
-		//do_action( 'woocommerce_account_content' );
-	?>
-</div>
+			if ( ! empty( $subscriptions ) ){
+				foreach ( $subscriptions as $subscription_id => $subscription ){
+					do_action( 'woocommerce_subscription_details_table', $subscription );
+				}
+				
+			}
+			//do_action( 'woocommerce_account_payment-methods_endpoint', 'woocommerce_account_payment_methods' );		
+			//do_action( 'woocommerce_account_orders_endpoint', 'woocommerce_account_orders' );
+			//do_action( 'woocommerce_account_add-payment-method_endpoint', 'woocommerce_account_add_payment_method' );
+			//do_action( 'woocommerce_account_view-order_endpoint', 'woocommerce_account_view_order' );
+
+
+		?>
+	</div>
+	<?php
+}
